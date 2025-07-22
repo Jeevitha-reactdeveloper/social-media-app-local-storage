52ed5c44f2a6a7a611fcb455a980c78f4f84ebe2
Jeevitha.N <bejeevitha@gmail.com>
2025-07-22 22:48:56 +0530

    changed responsive design

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,6 @@ import  { useContext } from 'react'
 import DataContext from './contextapi/DataContext'
 
 const Header = ({title}) => {
-  const {width} = useContext(DataContext)
   return (
     <header className='Header'>
         <h1>{title}</h1>
